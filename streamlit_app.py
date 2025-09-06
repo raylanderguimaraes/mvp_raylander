0239b6fe20cab5a646f063a6ddc75aa363730beb
Raylander Guimarães <92590610+raylanderguimaraes@users.noreply.github.com>
2025-09-05 21:19:00 -0300

    update project

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Projeto clouding computing")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Raylander Guimarães Ramos")
+
+st.sidebar:
+    st.["menu"]
